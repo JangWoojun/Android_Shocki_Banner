88bd7b4585e459cdf5d07f986ced863eb9bde85b
JangWoojun <woojun0107@naver.com>
2024-08-13 11:09:10 +0900

    feat: 스테이터스 바 투명화 구현

--- app/src/main/java/com/seogaemo/android_shocki_banner/MainActivity.kt
+++ app/src/main/java/com/seogaemo/android_shocki_banner/MainActivity.kt
@@ -1,164 +1,38 @@
 package com.seogaemo.android_shocki_banner
 
-import android.animation.ValueAnimator
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
-import android.view.ViewTreeObserver
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.seogaemo.android_shocki_banner.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private var bannerWidth: Int? = null
-
-    private val viewList: List<ConstraintLayout> by lazy {
-        listOf(binding.banner1, binding.banner2, binding.banner3, binding.banner4, binding.banner5)
-    }
-    private val textList: List<TextView> by lazy {
-        listOf(binding.banner1Text, binding.banner2Text, binding.banner3Text, binding.banner4Text, binding.banner5Text)
-    }
-    private val imageList: List<ImageView> by lazy {
-        listOf(binding.banner1Image, binding.banner2Image, binding.banner3Image, binding.banner4Image, binding.banner5Image)
-    }
-
-    private var index = 0
-    private var isLeft = true
-
-    private val handler = Handler()
-    private val runnable = object : Runnable {
-        override fun run() {
-            updateView()
-            handler.postDelayed(this, 5000)
-            isLeft = true
-        }
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(SystemBarStyle.dark(0xffffff))
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        initView()
-        handler.postDelayed(runnable, 5000)
-
-        binding.bannerView.setOnTouchListener(object: OnSwipeTouchListener(this@MainActivity) {
-            override fun onSwipeLeft() {
-                isLeft = true
-                handler.removeCallbacks(runnable)
-                handler.post(runnable)
-            }
-            override fun onSwipeRight() {
-                isLeft = false
-                handler.removeCallbacks(runnable)
-                handler.post(runnable)
-            }
-        })
+        setupWindowInsets()
     }
 
-    private fun initView() {
-        val viewWidth = dpToPx(16)
+    private fun setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val statusBars = insets.getInsets(WindowInsetsCompat.Type.statusBars())
 
-        viewList.forEachIndexed { index, it ->
-            if (index == 0) {
-                val layoutParams = it.layoutParams as LinearLayout.LayoutParams
-                layoutParams.width = viewWidth
-                layoutParams.weight = 1f
-                it.layoutParams = layoutParams
+            val statusBarHeight = statusBars.top
+            val navBarHeight = systemBars.bottom
 
-                textList[index].visibility = View.VISIBLE
-                imageList[index].clearColorFilter()
-
-                calculateWidth(it)
-            } else {
-                val layoutParams = it.layoutParams as LinearLayout.LayoutParams
-                layoutParams.width = viewWidth
-                layoutParams.weight = 0f
-                it.layoutParams = layoutParams
-
-                textList[index].visibility = View.INVISIBLE
-                imageList[index].setColorFilter(Color.parseColor("#80000000"))
-            }
+            binding.buttonLayout.setPadding(0, statusBarHeight, 0, navBarHeight)
+            insets
         }
-    }
-
-    private fun updateView() {
-        val previousViewWidth = dpToPx(16)
-        bannerWidth?.let { nextViewWidth ->
-            val previousView = viewList[index]
-            val previousLayoutParams = previousView.layoutParams as LinearLayout.LayoutParams
-            val previousAnimator = ValueAnimator.ofInt(nextViewWidth, previousViewWidth)
-            previousAnimator.addUpdateListener { animation ->
-                val animatedValue = animation.animatedValue as Int
-                previousLayoutParams.width = animatedValue
-                previousView.layoutParams = previousLayoutParams
-            }
-            previousAnimator.duration = 300
-            previousAnimator.start()
-
-            textList[index].visibility = View.INVISIBLE
-            imageList[index].setColorFilter(Color.parseColor("#80000000"))
-
-            updateIndex()
-
-            val nextView = viewList[index]
-            val nextLayoutParams = nextView.layoutParams as LinearLayout.LayoutParams
-            val nextAnimator = ValueAnimator.ofInt(previousViewWidth, nextViewWidth)
-            nextAnimator.addUpdateListener { animation ->
-                val animatedValue = animation.animatedValue as Int
-                if (animatedValue == nextViewWidth) {
-                    textList[index].visibility = View.VISIBLE
-                }
-                imageList[index].clearColorFilter()
-                nextLayoutParams.width = animatedValue
-                nextView.layoutParams = nextLayoutParams
-            }
-            nextAnimator.duration = 300
-            nextAnimator.start()
-        }
-    }
-
-    private fun updateIndex() {
-        if (isLeft) {
-            if (index + 1 < 5) {
-                index += 1
-            } else {
-                index = 0
-            }
-        } else {
-            if (index - 1 < 0) {
-                index = 4
-            } else {
-                index -= 1
-            }
-        }
-    }
-
-    private fun calculateWidth(view: View) {
-        view.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                view.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                bannerWidth = view.width
-            }
-        })
-    }
-
-    private fun dpToPx(dp: Int): Int {
-        val density = this.resources.displayMetrics.density
-        return (dp * density).toInt()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        handler.removeCallbacks(runnable)
     }
 
 }
