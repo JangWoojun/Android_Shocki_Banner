67e238a50f8299141c7cb8877ac98ad5ec67827c
JangWoojun <woojun0107@naver.com>
2024-08-13 10:54:49 +0900

    feat: ViewPagerAdapter 추가

---
+++ app/src/main/java/com/seogaemo/android_shocki_banner/ViewPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.seogaemo.android_shocki_banner
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.seogaemo.android_shocki_banner.databinding.BannerItemBinding
+
+class ViewPagerAdapter(private val datas: MutableList<String>):
+    RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerAdapter.ViewHolder {
+        val binding = BannerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerAdapter.ViewHolder, position: Int) {
+        holder.bind(datas[position])
+    }
+
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+
+    inner class ViewHolder(private val binding : BannerItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item : String){
+            binding.textView.text = item
+            binding.image.setImageResource(R.drawable.banner6)
+            binding.background.background.alpha = (0.4 * 255).toInt()
+        }
+    }
+}
